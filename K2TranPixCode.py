e62b6ced3279ddd0444e19002da0d7d0ea04e691
Ryan Ridden-Harper <ryanr@microobs1au.anu.edu.au>
2018-03-26 13:53:36 +1100

    further corrections

--- K2TranPixCode.py
+++ K2TranPixCode.py
@@ -620,7 +620,7 @@ def K2TranPixFig(Events,Eventtime,Eventmask,Data,Time,Frames,wcs,Save,File,Quali
             maxcolor = np.nanmax(Data[Eventtime[i][0]:Eventtime[i][-1],(Eventmask[i]==1)])
 
             Mid = np.where(Data[Eventtime[i][0]:Eventtime[i][-1],(Eventmask[i]==1)] == maxcolor)
-            if len(np.where(Eventmask[I]==1)[0]) > 1:
+            if len(np.where(Eventmask[i]==1)[0]) > 1:
                 position = np.where(Eventmask[i]==1)[Mid[1][0]]
             else:
                 position = np.where(Eventmask[i]==1)
