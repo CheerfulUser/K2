3c64bc48f383d6c7025b551ace3a8d828fd2f06c
Ryan Ridden-Harper <ryanr@microobs1au.anu.edu.au>
2018-06-04 11:44:27 +1000

    Updated the function to remove already calculated fields from the run.

--- MPI_K2_Wrap.py
+++ MPI_K2_Wrap.py
@@ -6,30 +6,6 @@ import time as sys_time
 # Import all the functions
 from K2TranPixCode import *
 
-field = 'c14'
-path = '/avatar/ryanr/Data/'+field+'/'
-Files = np.asarray(glob(path+'*.gz'))
-
-save = '/avatar/ryanr/Results/'
-
-Files = np.asarray(glob(path+'*.gz'))
-# Code to remove files from the list that have already been calculated
-#try:
-#    Log = open('/avatar/ryanr/Code/shell'+ field + '.out')
-#    log = Log.read()
-#    lines  = log.split('\n')
-#    files = []
-#    for line in lines:
-#        if '/avatar/ryanr/Data/' in line:
-#            print(line)
-#            files.append(line)
-#    for j in range(len(files)):
-#        Files = np.delete(Files,np.where(files[j] in Files))
-#except (FileNotFoundError):
-#    print('FileNotFoundError')
-
-dims = int(len(Files)) # set to be length of your task
-start = sys_time.time()
 
 def print_mpi(string):
     comm = MPI.COMM_WORLD
@@ -39,6 +15,37 @@ def print_master(string):
     comm = MPI.COMM_WORLD
     if comm.Get_rank() == 0:
         print("["+str(comm.Get_rank())+"] "+string)
+
+field = 'c14'
+path = '/avatar/ryanr/Data/'+field+'/'
+Files = np.asarray(glob(path+'*.gz'))
+
+save = '/avatar/ryanr/Results/'
+
+Files = np.asarray(glob(path+'*.gz'))
+# Code to remove files from the list that have already been calculated
+try:
+    Log = open('/avatar/ryanr/Code/shell' + field + '.out')
+    log = Log.read()
+    lines  = log.split('\n')
+    files = []
+    for line in lines:
+        if '/avatar/ryanr/Data/' in line:
+            print_mpi(line)
+            files.append(line)
+    for i in range(len(files)):
+        files[i] = files[i].split(' ')[1]
+        beep = set.intersection(set(files[i]), set('['))
+        if len(beep) > 0:
+            files[i] = files[i].split('[')[0]
+                   
+        Files = np.delete(Files,np.where(files[j] == Files))
+        
+except (FileNotFoundError):
+    print('No file')
+
+dims = int(len(Files)) # set to be length of your task
+start = sys_time.time()
 
 # init MPI
 
@@ -69,8 +76,9 @@ filename = "my_prog_"+str(myPE).zfill(4)
 
 for n in range(my_start, my_end+1):
     mytimestart = sys_time.time()
-    print(Files[n])
+    
     K2TranPix(Files[n],save)
+    print_mpi(Files[n])
     
     mytimestop = sys_time.time()
     mytime = mytimestop-mytimestart
