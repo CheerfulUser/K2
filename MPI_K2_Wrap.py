eab6969fa959dc8fc0972d384aafbdee6966536b
Ryan Ridden-Harper <ryanr@microobs1au.anu.edu.au>
2018-06-08 15:14:01 +1000

    Files should now be distributed to nodes based on equal size. This should prevent some nodes finishing much earlier than others.

--- MPI_K2_Wrap.py
+++ MPI_K2_Wrap.py
@@ -16,41 +16,7 @@ def print_master(string):
     if comm.Get_rank() == 0:
         print("["+str(comm.Get_rank())+"] "+string)
 
-field = 'c14'
-path = '/avatar/ryanr/Data/'+field+'/'
-Files = np.asarray(glob(path+'*.gz'))
-
-save = '/avatar/ryanr/Results/'
-
-Files = np.asarray(glob(path+'*.gz'))
-# Code to remove files from the list that have already been calculated
-try:
-    Log = open('/avatar/ryanr/Code/shell' + field + '.out')
-    log = Log.read()
-    lines  = log.split('\n')
-    files = []
-    for line in lines:
-        if '/avatar/ryanr/Data/' in line:
-            print_mpi(line)
-            files.append(line)
-    for i in range(len(files)):
-        files[i] = files[i].split(' ')[1]
-        beep = set.intersection(set(files[i]), set('['))
-        if len(beep) > 0:
-            files[i] = files[i].split('[')[0]
-                   
-        Files = np.delete(Files,np.where(files[i] == Files))
-        
-except (FileNotFoundError):
-    print('No file')
-
-print_mpi('Filesbloop ' + str(len(Files)))
-
-dims = int(len(Files)) # set to be length of your task
-start = sys_time.time()
-
 # init MPI
-
 comm = MPI.COMM_WORLD
 nPE = comm.Get_size()
 myPE = comm.Get_rank()
@@ -58,24 +24,73 @@ print_master("Total number of MPI ranks = "+str(nPE))
 comm.Barrier()
 
 
-# Progress saving function
+field = 'c01'
+path = '/avatar/ryanr/Data/'+field+'/'
+Files = np.asarray(glob(path+'*.gz'))
 
-def writemyprog(filename,out):
-    np.savez(filename,out)
+save = '/avatar/ryanr/Results/'
+
+Files = np.asarray(glob(path+'*.gz'))
+# Code to remove files from the list that have already been calculated
+if comm.Get_rank() == 0:
+    try:
+        Log = open('/avatar/ryanr/Code/shell' + field + '.out')
+        log = Log.read()
+        lines  = log.split('\n')
+        files = []
+        for line in lines:
+            if '/avatar/ryanr/Data/' in line:
+                print_mpi(line)
+                files.append(line)
+        for i in range(len(files)):
+            files[i] = files[i].split(' ')[1]
+            beep = set.intersection(set(files[i]), set('['))
+            if len(beep) > 0:
+                files[i] = files[i].split('[')[0]
+                       
+            Files = np.delete(Files,np.where(files[i] == Files))
+            
+    except (FileNotFoundError):
+        print('No file')
+
+    print_mpi('Filesbloop ' + str(len(Files)))
+
+Files = comm.bcast(Files, root = 0)
+
+if comm.Get_rank() == 0:
+    size = [] 
+    for i in range(len(Files)):
+        size.append(os.path.getsize(Files[i]))
+    size = np.array(size)
+    totalsize = np.nansum(size)
+    interval_size = totalsize / nPE
+
+    starts = np.zeros(nPE,dtype=int)
+    for i in range(nPE):
+        if i == 0:
+            starts[i] = 0
+        else:
+            sumsize = 0
+            j = 1
+            while sumsize <= interval_size:
+                sumsize = np.nansum(size[starts[i-1]:starts[i-1]+j])
+                j += 1
+            starts[i] = starts[i-1] + (j - 1)
+
+starts = comm.bcast(starts, root = 0)
+
+
+dims = int(len(Files)) # set to be length of your task
+start = sys_time.time()
 
 # domain decomposition
-
-my_start = int(myPE * (dims / nPE));
-my_end   = int((myPE+1) * (dims / nPE) - 1);
+my_start = starts[myPE]#int(myPE * (dims / nPE));
+my_end   = starts[myPE + 1] - 1 #int((myPE+1) * (dims / nPE) - 1);
 # last PE gets the rest
 if (myPE == nPE-1): my_end = dims-1;
 print_mpi("my_start = "+str(my_start)+", my_end = "+str(my_end))
 
 # parallel loop
-
-filename = "my_prog_"+str(myPE).zfill(4)
-
-
 for n in range(my_start, my_end+1):
     mytimestart = sys_time.time()
     
@@ -84,7 +99,6 @@ for n in range(my_start, my_end+1):
     
     mytimestop = sys_time.time()
     mytime = mytimestop-mytimestart
-    #print('n=%g' %n, 'my_time=%f' %mytime)
 
 
 # MPI collective communication (all reduce)
