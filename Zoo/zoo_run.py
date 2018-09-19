9783766c6cb007c2f9217874a89655722b518383
Ryan Ridden-Harper <ryanr@microobs1au.anu.edu.au>
2018-09-19 11:30:29 +1000

    only galaxies and qso

--- Zoo/zoo_run.py
+++ Zoo/zoo_run.py
@@ -1,10 +1,11 @@
 from Zoo_upload_tools import *
 
-permit = ['Unknown', 'G', 'QSO', 'R', 'IrS', 'WDStar', 'SN', 'XrayS']
+#permit = ['Unknown', 'G', 'QSO', 'R', 'IrS', 'WDStar', 'SN', 'XrayS']
+permit = ['G', 'QSO']
 
 prohibit = ['Star']
 
-set_name = 'c17_test'
+set_name = 'c17_test_G_QSO'
 
 path = '/export/maipenrai2/skymap/brad/KEGS/Data/K2BS/Results/c17/'
 save = '/export/maipenrai2/skymap/brad/KEGS/Data/K2BS/Results/c17/Zoo'
