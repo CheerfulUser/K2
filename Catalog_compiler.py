2dbadd7268f08aac9fa5868f29a1565e5bf3724e
Ryan Ridden-Harper <ryanr@Ryans-MacBook-Pro-4.local>
2018-12-22 23:52:32 +1100

    sets now used to make checking go fast, and runs on all campaign simultaneously

--- Catalog_compiler.py
+++ Catalog_compiler.py
@@ -5,24 +5,28 @@ import os
 import csv
 
 def Join_searches(Path,Save):
-    files = glob(Path + '*.csv')
-    savename = Save + 'NED_' + files[0].split('_')[2] + '.csv'
-    objects = []
-    for file in files:
-        dataframe = pd.read_csv(file)
-        data = dataframe.values
-        for row in data:
-            if row[1] not in objects:
-                objects += [row[1]]
-                if os.path.isfile(savename):
-                        with open(savename, 'a') as csvfile:
+    camps = glob(path + '*/')
+    for camp in camps:
+        files = glob(camp + '*.csv')
+        savename = Save + 'NED_' + files[0].split('_')[2] + '.csv'
+        objects = set([])
+        for file in files:
+            print(file)
+            dataframe = pd.read_csv(file)
+            data = dataframe.values
+            for row in data:
+                if row[2] not in objects:
+                    objects.add(row[2])
+                    if os.path.isfile(savename):
+                            with open(savename, 'a') as csvfile:
+                                spamwriter = csv.writer(csvfile, delimiter=',')
+                                spamwriter.writerow(row[2:])
+                    else:
+                        with open(savename, 'w') as csvfile:
                             spamwriter = csv.writer(csvfile, delimiter=',')
-                            spamwriter.writerow(row)
+                            spamwriter.writerow(dataframe.keys()[2:])
+                            spamwriter.writerow(row[2:])
                 else:
-                    with open(savename, 'w') as csvfile:
-                        spamwriter = csv.writer(csvfile, delimiter=',')
-                        spamwriter.writerow(dataframe.keys())
-                        spamwriter.writerow(row)
-            else:
-                pass
-    return 'Done NED cat for C' + files[0].split('_')[2]
+                    pass
+        print('Done NED cat for C' + files[0].split('_')[2])
+    return objects, 'Done NED'
